af0a0b98918d80bc07413d67a6f62ffe75f85560
Kidjapa <brunoluizk@gmail.com>
2019-11-04 20:26:00 -0300

    Adicionando envirronments para produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  site_url: "http://praia2019.webnofear.com.br",
+  // api_url: "http://localhost:9000",
+  api_url: "http://apipraia.webnofear.com.br"
 };
